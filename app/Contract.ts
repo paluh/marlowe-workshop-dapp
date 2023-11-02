c7ef3cebc5bf646593eb830c907fea673b461433
Tomasz Rybarczyk <tomasz.rybarczyk@iohk.io>
2023-11-02 10:25:23 +0100

    Add contract generation

---
+++ app/Contract.ts
@@ -0,0 +1,25 @@
+import { Address, Contract, datetoTimeout } from "@marlowe.io/language-core-v1";
+import { lovelace } from "@marlowe.io/language-core-v1/playground-v1";
+
+export const mkSimpleDeposit = (acceptor: Address, donor: Address): Contract => {
+  const twentyFourHoursInMilliseconds = 24 * 60 * 60 * 1000;
+  const inTwentyFourHours = datetoTimeout(new Date(Date.now() + twentyFourHoursInMilliseconds));
+
+  return {
+      "when": [
+        {
+          "then": "close",
+          "case": {
+            party: donor,
+            deposits: 1n,
+            of_token: lovelace,
+            into_account: acceptor
+          }
+        }
+      ],
+      timeout_continuation: "close",
+      timeout: inTwentyFourHours
+  };
+};
+
+
